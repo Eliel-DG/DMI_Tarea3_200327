a12e93e7b6848213ecf7ccf09541a7543f8fd19e
elieldiazgalindo <eliel.diazgalindo@utxicotepec.edu.mx>
2023-09-08 13:18:29 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/tarea3_200327/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tarea3_200327
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+// Creado por: Eliel Diaz Galindo
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
+// Importamos la biblioteca de Flutter para utilizar widgets y herramientas de interfaz de usuario.
+import 'package:flutter/material.dart';
+
+// La función principal que inicia la aplicación.
+void main() {
+  // Lanzamos la aplicación con el widget MyApp.
+  runApp(const MyApp());
+}
+
+// Definición de la clase MyApp, que extiende StatelessWidget.
+class MyApp extends StatelessWidget {
+  // Constructor constante para la clase MyApp.
+  const MyApp({super.key});
+
+  // Implementación del método build, que construye y devuelve el widget principal de la aplicación.
+  @override
+  Widget build(BuildContext context) {
+    // Creamos un MaterialApp, que configura aspectos globales de la aplicación.
+    return MaterialApp(
+      title: ' Tarea3_200327', // Título de la aplicación.
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(), // Widget de inicio de la aplicación.
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TAREA 3: 200327',
+            style: TextStyle(color: Color.fromARGB(255, 48, 47, 47))),
+        backgroundColor: Color.fromARGB(255, 74, 163, 179),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 57, 100, 136), // Cambia el fondo del body a azul.
+        child: Center(
+          // Centro el contenido vertical y horizontalmente
+          child: Padding(
+            padding: EdgeInsets.all(
+                16.0), // Agrega un relleno de unidades en todos los lados
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Centra verticalmente
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    alignment: Alignment.center, // Centra horizontalmente
+                    child: Text(
+                      'TAREA 3 200327 :Estilización y uso de Fuentes',
+                      style: TextStyle(
+                        fontFamily:
+                            'Wash', // Usa el nombre de tu fuente personalizada.
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ), // Agrega un espacio vertical entre el texto y la imagen.
+                Image.asset(
+                  'assets/image.jpg', //  ruta de la imagen.
+                  width: 300, // Define el ancho de la imagen.
+                  height: 310, // Define la altura de la imagen.
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
